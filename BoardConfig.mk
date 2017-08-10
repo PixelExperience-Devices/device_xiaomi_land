9e0f5e35ad6c08cf396cf08e11a2d7c9deeb1a8b
Martijn Coenen <maco@google.com>
2017-08-10 17:15:00 +0200

    land: Add selinux/private/service_contexts.
    This makes sure that the remaining binder services
    end up in plat_service_contexts.
    
    That in turn allows us to enforce that servicemanager
    will only serve services from plat_service_contexts
    on FULL_TREBLE devices.
    
    Bug: 36866029
    Test: boot, verify radio services still work
    Change-Id: Ib67b3a03e5599484c5c4fb27a0f323a37dd51636

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -233,6 +233,7 @@ TARGET_RECOVERY_FSTAB 		 := $(DEVICE_PATH)/rootdir/fstab.qcom
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 #Treble
