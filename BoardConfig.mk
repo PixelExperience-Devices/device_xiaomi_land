da386bfbdf05b3103555679c87a1a934f9a88f21
Sridhar Parasuram <sridhar@codeaurora.org>
2018-03-01 10:33:47 -0800

    land: Move vendor specific policies inside vendor directory
    Change-Id: I44ab77d2572438cd9d45aa4b6dc8fb92d0b9a12a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -234,7 +234,7 @@ TARGET_RECOVERY_FSTAB 		 := $(DEVICE_PATH)/rootdir/fstab.qcom
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 #Treble
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
