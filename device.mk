96f87c1102567d9859281cd90df8cf96cd74f226
Shibin George <shibing@codeaurora.org>
2018-06-21 17:09:33 +0530

    land: Enable BOARD_VNDK_VERSION flag
    Change-Id: I457aa69c604dc63570c60c416e06885a73bbaac3

--- device.mk
+++ device.mk
@@ -399,10 +399,9 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
-# VNDK-SP
+# VNDK
 PRODUCT_PACKAGES += \
-    vndk-sp \
-    vndk-ext
+    vndk_package
 
 # WiFi
 PRODUCT_PACKAGES += \
