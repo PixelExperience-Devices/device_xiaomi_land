43c47739d14dffe1da18ed7c22479dbecdf17bb7
LuK1337 <priv.luk@gmail.com>
2018-06-29 18:44:41 +0200

    land: Remove camera.device@3.2-impl from PRODUCT_PACKAGES
    * camera.device@1.0-impl & camera.device@3.2-impl are dependency of camera provider
      therefore there's no need to add it to PRODUCT_PACKAGES.
    
    Change-Id: I620e13349829112f541c7c61d9f0cac2403ebf74

--- device.mk
+++ device.mk
@@ -130,8 +130,6 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    camera.device@1.0-impl \
-    camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
