af02c0d60c9deb42eee867d798341c031c145d0b
Bruno Martins <bgcngm@gmail.com>
2018-07-07 22:40:59 +0100

    land: Remove implicitly built QTI camera shared libraries
     * These shared libs are built when TARGET_USES_QTI_CAMERA_DEVICE
       flag is enabled.
    
    Change-Id: I1b497fe2b55776078398909d83725a1a83695275

--- device.mk
+++ device.mk
@@ -132,8 +132,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor \
     camera.msm8937 \
     libmm-qcamera \
     GCam
