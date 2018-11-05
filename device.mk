2914c1432eb4af1197d0a9139b93584fa0d890a9
Jeff Tinker <jtinker@google.com>
2018-11-05 15:01:48 -0800

    land: Enable clearkey drm plugin v1.2
    test: android.media.cts.MediaDrmClearkeyTest#testOfflineKeyManagement
    
    bug: 117570956
    bug: 110838441
    bug: 116252891
    Change-Id: I86fedc9d383f90cf874c8001f7064b9bfbbe3658

--- device.mk
+++ device.mk
@@ -175,7 +175,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.1-service.clearkey \
+    android.hardware.drm@1.2-service.clearkey \
     libprotobuf-cpp-lite
 
 # Fingerprint
