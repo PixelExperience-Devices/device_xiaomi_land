1d89fdf4387b4e9b57f99edbb405cafb7e9c099e
mike dooley <mdooley@google.com>
2018-12-18 20:29:02 +0100

    land: Enabling sound trigger V2_2
    V2_2 has the same functionality as V2_1 except it adds one new
    api, getModelState.
    
    Note: V2_1 will be removed in a subsequent cl.
    Change-Id: Ieda60178e274c8c7c8d9ee736a59b09f4e21c75d

--- device.mk
+++ device.mk
@@ -90,7 +90,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-service \
     android.hardware.audio.effect@5.0 \
     android.hardware.audio.effect@5.0-impl \
-    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.2-service \
     audio.a2dp.default \
     audio.primary.msm8937 \
     audio.r_submix.default \
