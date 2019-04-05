2b2f2b63be60d838ee84104679ed88066a5ade9a
Artem Borisov <dedsa2002@gmail.com>
2019-04-05 19:52:44 +0530

    land: Enable some UX optimizations from stock
    * From msm8953
    * Its really effective (Fixes little lag here & there)
    
    Change-Id: I6f3ae240366cc49e1a25d2f76ddd847ced0fb8f4

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -211,6 +211,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.sys.fw.trim_cache_percent=100 \
     ro.vendor.qti.sys.fw.trim_enable_memory=2147483648
 
+# UI
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.use_fifo_ui=1
+
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0
