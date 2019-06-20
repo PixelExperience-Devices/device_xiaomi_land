4f2810583b8ef4b72d36fe08e2308086c8d3aa3f
JacobDeepu <d2jdeepu@gmail.com>
2019-06-20 12:28:52 +0000

    land: Prevent cleanup of unused fp

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -85,7 +85,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Fingerprint
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.qfp=false
+    persist.qfp=false \
+    ro.fingerprint.cleanup.unused=false
 
 # FM
 PRODUCT_PROPERTY_OVERRIDES += \
