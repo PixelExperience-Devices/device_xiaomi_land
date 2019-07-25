dfa65d52f019128cc69c8a62697ad1d18352c7d5
Dede Dindin Qudsy <xtrymind@gmail.com>
2019-07-25 12:43:35 +0200

    land: Set BT_CLEAN_TURN_ON_DISABLED flag to TRUE
    * Looks like BT_CLEAN_TURN_ON_DISABLED is already deprecated in Oreo but
    even Google still includes this on Pixel 3, either it's defined somewhere
    else or Google forgot to remove it. Make it same as Pixel, using TRUE value
    rather than 1
    
    Signed-off-by: David Trpchevski <trpcevski.david@gmail.com>

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -28,7 +28,7 @@
 #define BTIF_HF_WBS_PREFERRED TRUE
 
 // skips conn update at conn completion
-#define BT_CLEAN_TURN_ON_DISABLED 1
+#define BT_CLEAN_TURN_ON_DISABLED TRUE
 
 /* Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec */
 #define AVDT_NUM_SEPS 12
