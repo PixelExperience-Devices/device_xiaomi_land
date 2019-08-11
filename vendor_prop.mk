a8032d8196825797f1d0551e73044ed3b5e42a10
JacobDeepu <d2jdeepu@gmail.com>
2019-08-11 13:31:07 +0530

    land: Update vendor properties

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -1,210 +1,194 @@
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
-vendor.voice.playback.conc.disabled=true \
-vendor.voice.record.conc.disabled=false \
-vendor.voice.voip.conc.disabled=true \
-vendor.voice.conc.fallbackpath=deep-buffer \
-vendor.audio.parser.ip.buffer.size=0 \
-vendor.audio_hal.period_size=192 \
-ro.vendor.audio.sdk.ssr=false \
-ro.vendor.audio.sdk.fluencetype=fluence \
-persist.vendor.audio.fluence.voicecall=true \
-persist.vendor.audio.fluence.voicerec=true \
-persist.vendor.audio.fluence.speaker=true \
-audio.offload.disable=true \
-vendor.audio.tunnel.encode=false \
-vendor.audio.offload.buffer.size.kb=64 \
-audio.offload.min.duration.secs=30 \
-audio.offload.video=true \
-vendor.audio.offload.track.enable=true \
-audio.deep_buffer.media=true \
-vendor.audio.playback.mch.downsample=true \
-vendor.voice.path.for.pcm.voip=true \
-vendor.audio.use.sw.alac.decoder=true \
-vendor.audio.use.sw.ape.decoder=true \
-vendor.audio.offload.gapless.enabled=true \
-vendor.audio.offload.multiple.enabled=false \
-vendor.audio.safx.pbe.enabled=true \
-vendor.audio.pp.asphere.enabled=false \
-vendor.audio.dolby.ds2.enabled=true \
-ro.af.client_heap_size_kbyte=7168 \
-af.fast_track_multiplier=1 \
-persist.vendor.audio.speaker.prot.enable=false \
-vendor.audio.offload.multiaac.enable=true \
-vendor.audio.dolby.ds2.hardbypass=true \
-vendor.audio.hw.aac.encoder=true \
-vendor.audio.flac.sw.decoder.24bit=true \
-ro.config.vc_call_vol_steps=8 \
-ro.config.media_vol_steps=20
+    vendor.voice.playback.conc.disabled=true \
+    vendor.voice.record.conc.disabled=false \
+    vendor.voice.voip.conc.disabled=true \
+    vendor.voice.conc.fallbackpath=deep-buffer \
+    vendor.audio.parser.ip.buffer.size=0 \
+    vendor.audio_hal.period_size=192 \
+    ro.vendor.audio.sdk.ssr=false \
+    ro.vendor.audio.sdk.fluencetype=fluence \
+    persist.vendor.audio.fluence.voicecall=true \
+    persist.vendor.audio.fluence.voicerec=true \
+    persist.vendor.audio.fluence.speaker=true \
+    audio.offload.disable=true \
+    vendor.audio.tunnel.encode=false \
+    vendor.audio.offload.buffer.size.kb=64 \
+    audio.offload.min.duration.secs=30 \
+    audio.offload.video=true \
+    vendor.audio.offload.track.enable=true \
+    audio.deep_buffer.media=true \
+    vendor.audio.playback.mch.downsample=true \
+    vendor.voice.path.for.pcm.voip=true \
+    vendor.audio.use.sw.alac.decoder=true \
+    vendor.audio.use.sw.ape.decoder=true \
+    vendor.audio.offload.gapless.enabled=true \
+    vendor.audio.offload.multiple.enabled=false \
+    vendor.audio.safx.pbe.enabled=true \
+    vendor.audio.pp.asphere.enabled=false \
+    vendor.audio.dolby.ds2.enabled=true \
+    ro.af.client_heap_size_kbyte=7168 \
+    af.fast_track_multiplier=1 \
+    persist.vendor.audio.speaker.prot.enable=false \
+    vendor.audio.offload.multiaac.enable=true \
+    vendor.audio.dolby.ds2.hardbypass=true \
+    vendor.audio.hw.aac.encoder=true \
+    vendor.audio.flac.sw.decoder.24bit=true \
+    ro.config.vc_call_vol_steps=8 \
+    ro.config.media_vol_steps=20
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
-persist.vendor.bluetooth.modem_nv_support=true \
-persist.vendor.bt.enable.splita2dp=false \
-vendor.qcom.bluetooth.soc=smd
+    persist.vendor.bluetooth.modem_nv_support=true \
+    persist.vendor.bt.enable.splita2dp=false \
+    vendor.qcom.bluetooth.soc=smd
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-vendor.camera.hal1.packagelist=com.skype.raider,com.google.android.talk \
-persist.vendor.camera.display.umax=2560x1440 \
-persist.vendor.camera.display.lmax=1280x720 \
-persist.camera.is_type=2 \
-persist.camera.HAL3.enabled=1 \
-persist.ts.postmakeup=true \
-persist.ts.rtmakeup=true
+    persist.camera.is_type=1 \
+    persist.ts.postmakeup=true \
+    persist.ts.rtmakeup=true \
+    persist.vendor.camera.display.lmax=1280x720 \
+    persist.vendor.camera.display.umax=1920x1080 \
+    persist.vendor.qti.telephony.vt_cam_interface=1 \
+    vendor.camera.hal1.packagelist=com.skype.raider,com.google.android.talk
 
 # CNE/DPM
 PRODUCT_PROPERTY_OVERRIDES += \
-persist.vendor.cne.feature=1
+    persist.vendor.cne.feature=1
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.opengles.version=196610 \
-ro.sf.lcd_density=320 \
-debug.sf.enable_hwc_vds=1 \
-debug.sf.hw=0 \
-debug.sf.latch_unsignaled=1 \
-debug.egl.hw=0 \
-persist.hwc.mdpcomp.enable=true \
-debug.mdpcomp.logs=0 \
-dev.pm.dyn_samplingrate=1 \
-persist.demo.hdmirotationlock=false \
-debug.enable.sglscale=1 \
-debug.sf.recomputecrop=0 \
-sdm.debug.disable_skip_validate=1 \
-vendor.display.enable_default_color_mode=1 \
-vendor.display.disable_skip_validate=1 \
-persist.debug.wfd.enable=1 \
-persist.hwc.enable_vds=1 \
-debug.sdm.support_writeback=0
+    ro.opengles.version=196610 \
+    ro.sf.lcd_density=280 \
+    debug.sf.enable_hwc_vds=1 \
+    debug.sf.hw=0 \
+    debug.sf.latch_unsignaled=1 \
+    debug.egl.hw=0 \
+    persist.hwc.mdpcomp.enable=true \
+    debug.mdpcomp.logs=0 \
+    dev.pm.dyn_samplingrate=1 \
+    persist.demo.hdmirotationlock=false \
+    debug.enable.sglscale=1 \
+    debug.sf.recomputecrop=0 \
+    sdm.debug.disable_skip_validate=1 \
+    vendor.display.enable_default_color_mode=1 \
+    vendor.display.disable_skip_validate=1 \
+    persist.debug.wfd.enable=1 \
+    persist.hwc.enable_vds=1 \
+    debug.sdm.support_writeback=0
 
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
-drm.service.enabled=true
+    drm.service.enabled=true
 
 # Fingerprint
 PRODUCT_PROPERTY_OVERRIDES += \
-persist.qfp=false
+    persist.qfp=false
 
 # FM
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.fm.transmitter=false
+    ro.fm.transmitter=false
 
 # FRP
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.frp.pst=/dev/block/bootdevice/by-name/config
+    ro.frp.pst=/dev/block/bootdevice/by-name/config
 
 # GPS
 PRODUCT_PROPERTY_OVERRIDES += \
-persist.gps.qc_nlp_in_use=1 \
-persist.loc.nlp_name=com.qualcomm.location \
-ro.gps.agps_provider=1
-
-# ro.location properties below are used in conjunction
-# with NLP_MODE setting in /vendor/etc/izat.conf
-# either as the main network provider or as a fallback
-# provider depending on NLP_MODE that is set
-# These properties are optional and will not cause
-# issues if not set to existing packages.
-# ro.location.osnlp.package sets the name of default
-# OS sepecific network location provider package to use
-# ro.location.osnlp.region.package is set to the name
-# of the alternative network location provider package
-# to be used for a particular region where the default
-# network location provider is not functional
-PRODUCT_PROPERTY_OVERRIDES += \
-ro.location.osnlp.package=com.google.android.gms \
-ro.location.osnlp.region.package=
-
-# Shutdown timeout
-PRODUCT_PROPERTY_OVERRIDES += \
-sys.vendor.shutdown.waittime=500 \
-ro.build.shutdown_timeout=0
-
-# SdcardFs
-PRODUCT_PROPERTY_OVERRIDES += \
-ro.sys.sdcardfs=true
+    persist.gps.qc_nlp_in_use=1 \
+    persist.loc.nlp_name=com.qualcomm.location \
+    ro.gps.agps_provider=1
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
-media.msm8956hw=0 \
-mm.enable.smoothstreaming=true \
-mmp.enable.3g2=true \
-media.aac_51_output_enabled=true \
-vendor.mm.enable.qcom_parser=1048575 \
-vendor.vidc.enc.narrow.searchrange=1 \
-vendor.vidc.disable.split.mode=1 \
-vendor.video.disable.ubwc=1
+    media.msm8956hw=0 \
+    mm.enable.smoothstreaming=true \
+    mmp.enable.3g2=true \
+    media.aac_51_output_enabled=true \
+    vendor.mm.enable.qcom_parser=1048575 \
+    vendor.vidc.enc.narrow.searchrange=1 \
+    vendor.vidc.disable.split.mode=1 \
+    vendor.video.disable.ubwc=1
 
-#netmgrd
+# Netmgrd
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.vendor.use_data_netmgrd=true \
-persist.data.netmgrd.qos.enable=true \
-ro.use_data_netmgrd=true \
-persist.vendor.data.mode=concurrent
+    ro.vendor.use_data_netmgrd=true \
+    persist.data.netmgrd.qos.enable=true \
+    persist.vendor.data.mode=concurrent
+
+# Nitz
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.rild.nitz_plmn="" \
+    persist.rild.nitz_long_ons_0="" \
+    persist.rild.nitz_long_ons_1="" \
+    persist.rild.nitz_long_ons_2="" \
+    persist.rild.nitz_long_ons_3="" \
+    persist.rild.nitz_short_ons_0="" \
+    persist.rild.nitz_short_ons_1="" \
+    persist.rild.nitz_short_ons_2="" \
+    persist.rild.nitz_short_ons_3=""
 
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.vendor.qti.am.reschedule_service=true \
-ro.vendor.qti.core_ctl_min_cpu=2 \
-ro.vendor.qti.core_ctl_max_cpu=4 \
-ro.vendor.at_library=libqti-at.so \
-ro.vendor.extension_library=libqti-perfd-client.so \
-ro.vendor.gt_library=libqti-gt.so
+    ro.sys.fw.dex2oat_thread_count=8 \
+    ro.vendor.qti.am.reschedule_service=true \
+    ro.vendor.qti.core_ctl_min_cpu=2 \
+    ro.vendor.qti.core_ctl_max_cpu=4 \
+    ro.vendor.at_library=libqti-at.so \
+    ro.vendor.extension_library=libqti-perfd-client.so \
+    ro.vendor.gt_library=libqti-gt.so
 
 # Perf configuration
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.vendor.qti.sys.fw.bservice_enable=true \
-ro.vendor.qti.sys.fw.bservice_limit=5 \
-ro.vendor.qti.sys.fw.bservice_age=5000 \
-ro.vendor.qti.sys.fw.use_trim_settings=true \
-ro.vendor.qti.sys.fw.empty_app_percent=50 \
-ro.vendor.qti.sys.fw.trim_empty_percent=100 \
-ro.vendor.qti.sys.fw.trim_cache_percent=100 \
-ro.vendor.qti.sys.fw.trim_enable_memory=2147483648
+    ro.vendor.qti.sys.fw.bservice_enable=true \
+    ro.vendor.qti.sys.fw.bservice_limit=5 \
+    ro.vendor.qti.sys.fw.bservice_age=5000 \
+    ro.vendor.qti.sys.fw.use_trim_settings=true \
+    ro.vendor.qti.sys.fw.empty_app_percent=50 \
+    ro.vendor.qti.sys.fw.trim_empty_percent=100 \
+    ro.vendor.qti.sys.fw.trim_cache_percent=100 \
+    ro.vendor.qti.sys.fw.trim_enable_memory=2147483648
 
-#system props for time-services
+# Radio
 PRODUCT_PROPERTY_OVERRIDES += \
-persist.timed.enable=true
+    DEVICE_PROVISIONED=1 \
+    service.qti.ims.enabled=1 \
+    persist.radio.ims.cmcc=true \
+    persist.radio.calls.on.ims=true \
+    persist.radio.jbims=1 \
+    persist.radio.VT_ENABLE=1 \
+    persist.radio.VT_HYBRID_ENABLE=1 \
+    persist.radio.schd.cache=3500 \
+    persist.data.iwlan.enable=true \
+    persist.dbg.ims_volte_enable=1 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=0 \
+    persist.radio.multisim.config=dsds \
+    persist.vendor.radio.aosp_usr_pref_sel=true \
+    persist.vendor.radio.apm_sim_not_pwdn=1 \
+    persist.vendor.radio.custom_ecc=1 \
+    persist.vendor.radio.prefer_spn=1 \
+    persist.vendor.radio.rat_on=combine \
+    persist.vendor.radio.sib16_support=1 \
+    ril.subscription.types=NV,RUIM \
+    rild.libargs=-d/dev/smd0 \
+    rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
+    ro.telephony.call_ring.multiple=false \
+    ro.telephony.default_network=20,20 \
+    vendor.service.qti.ims.enabled=1 \
+    telephony.lteOnCdmaDevice=1
 
-# IMS
+# Shutdown timeout
 PRODUCT_PROPERTY_OVERRIDES += \
-service.qti.ims.enabled=1 \
-persist.radio.ims.cmcc=true \
-persist.radio.calls.on.ims=true \
-persist.radio.jbims=1 \
-persist.radio.VT_ENABLE=1 \
-persist.radio.VT_HYBRID_ENABLE=1 \
-persist.radio.schd.cache=3500 \
-persist.dbg.ims_volte_enable=1 \
-persist.dbg.volte_avail_ovr=1 \
-persist.dbg.vt_avail_ovr=1 \
-persist.radio.videopause.mode=1 \
-persist.data.iwlan.enable=true \
-persist.vendor.qti.telephony.vt_cam_interface=1
+    sys.vendor.shutdown.waittime=500 \
+    ro.build.shutdown_timeout=0
 
-# RIL
+# SdcardFs
 PRODUCT_PROPERTY_OVERRIDES += \
-DEVICE_PROVISIONED=1 \
-ro.telephony.call_ring.multiple=false \
-ro.telephony.default_network=20,20 \
-rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
-rild.libargs=-d /dev/smd0 \
-persist.rild.nitz_plmn= \
-persist.rild.nitz_long_ons_0= \
-persist.rild.nitz_long_ons_1= \
-persist.rild.nitz_long_ons_2= \
-persist.rild.nitz_long_ons_3= \
-persist.rild.nitz_short_ons_0= \
-persist.rild.nitz_short_ons_1= \
-persist.rild.nitz_short_ons_2= \
-persist.rild.nitz_short_ons_3= \
-persist.radio.multisim.config=dsds \
-persist.vendor.radio.apm_sim_not_pwdn=1 \
-persist.vendor.radio.custom_ecc=1 \
-persist.vendor.radio.rat_on=combine \
-persist.vendor.radio.report_codec=1 \
-persist.vendor.radio.sib16_support=1 \
-ril.ecclist=000,08,100,101,102,110,112,118,119,120,122,911,999 \
-ril.subscription.types=NV,RUIM \
-telephony.lteOnCdmaDevice=1
+    ro.sys.sdcardfs=true
+
+# Time Services
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.timed.enable=true
