5a1dc7fb1c7db8c17aacb20b79b0dff3b78a820b
JacobDeepu <d2jdeepu@gmail.com>
2019-09-13 08:15:50 +0530

    Revert "land: Add gcam as prebuild camera app"
    This reverts commit b0d5c248c170934a1fb5af521812b7884431de87.

--- device.mk
+++ device.mk
@@ -133,7 +133,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor \
     camera.msm8937 \
-    GCam
+    Snap
 
 # Configstore
 PRODUCT_PACKAGES += \
