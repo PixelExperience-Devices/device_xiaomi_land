734b1e637bac753418ed7b683ce0011b905466f4
Michael Bestas <mkbestas@lineageos.org>
2019-09-26 01:56:06 +0300

    land: Switch to device/qcom/sepolicy-legacy-um policy
    Change-Id: I09ee12755ab0a1f54a9dca4b92313ef1127db3d6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -228,7 +228,7 @@ TARGET_USES_OLD_MNC_FORMAT := true
 TARGET_RECOVERY_FSTAB 		 := $(DEVICE_PATH)/rootdir/fstab.qcom
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 #Treble
