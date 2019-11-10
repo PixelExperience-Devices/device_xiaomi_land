4d54c68c228b1af1b1aa812747906a97542fa2d7
JacobDeepu <d2jdeepu@gmail.com>
2019-11-10 12:39:47 +0000

    land: rootdir: Tune post boot script

--- rootdir/init.qcom.post_boot.sh
+++ rootdir/init.qcom.post_boot.sh
@@ -190,32 +190,32 @@ function 8937_sched_dcvs_hmp()
     # enable governor for perf cluster
     echo 1 > /sys/devices/system/cpu/cpu0/online
     echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-    echo "19000 1094400:39000" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay
+    echo "19000" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay
     echo 85 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load
     echo 20000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate
     echo 1094400 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq
     echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/io_is_busy
-    echo "1 960000:85 1094400:90 1344000:80" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads
+    echo "45 960000:85" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads
     echo 40000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time
-    echo 40000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/sampling_down_factor
 
     # enable governor for power cluster
     echo 1 > /sys/devices/system/cpu/cpu4/online
     echo "interactive" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
     echo 39000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay
-    echo 90 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load
-    echo 20000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate
+    echo 85 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load
+    echo 40000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate
     echo 768000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/hispeed_freq
     echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/io_is_busy
-    echo "1 768000:90" > /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads
+    echo "45 768000:85" > /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads
     echo 40000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time
-    echo 40000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/sampling_down_factor
 
     # Enable sched guided freq control
     echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_sched_load
     echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_migration_notif
+    echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/enable_prediction
     echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_sched_load
     echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_migration_notif
+    echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/enable_prediction
     echo 50000 > /proc/sys/kernel/sched_freq_inc_notify
     echo 50000 > /proc/sys/kernel/sched_freq_dec_notify
 
@@ -2121,8 +2121,10 @@ case "$target" in
                 echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
 
                 # HMP scheduler (big.Little cluster related) settings
-                echo 93 > /proc/sys/kernel/sched_upmigrate
-                echo 83 > /proc/sys/kernel/sched_downmigrate
+                echo 9 > /proc/sys/kernel/sched_upmigrate_min_nice
+                echo 85 > /proc/sys/kernel/sched_spill_load
+                echo 85 > /proc/sys/kernel/sched_upmigrate
+                echo 85 > /proc/sys/kernel/sched_downmigrate
 
                 # re-enable thermal core_control
                 echo 1 > /sys/module/msm_thermal/core_control/enabled
