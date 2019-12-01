d3b1a601e9f78c1e30932e1a415e1ceeaa9818a0
Michael Bestas <mkbestas@lineageos.org>
2019-12-01 22:27:56 +0200

    land: wfd: Remove persist.debug.wfd.enable
    * Not needed, we enable it via overlay
    
    Change-Id: Ib4201136bd452f5cfec534587689eaa52bdf4a7f

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -74,7 +74,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     sdm.debug.disable_skip_validate=1 \
     vendor.display.enable_default_color_mode=1 \
     vendor.display.disable_skip_validate=1 \
-    persist.debug.wfd.enable=1 \
     persist.hwc.enable_vds=1 \
     debug.sdm.support_writeback=0
 
