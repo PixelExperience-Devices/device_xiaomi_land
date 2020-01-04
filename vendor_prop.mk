ab5fde2b73fc884e115ec339fdbb7b71c87a8ba1
Michael Bestas <mkbestas@lineageos.org>
2020-01-04 15:40:59 +0200

    land: Disable DPM features
    * Stops it from crashing, most devices have it disabled anyway.
    
    Change-Id: If5f9e94f99d7c22b2f057dd1785c45186029b023

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -55,7 +55,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # CNE/DPM
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.cne.feature=1
+    persist.vendor.cne.feature=1 \
+    persist.vendor.dpm.feature=0
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
