e0e957ff29142552ea5111bce41ef6608d55edb8
sreeharimkl <50312757+sreeharimkl@users.noreply.github.com>
2020-01-12 09:41:17 +0530

    Bring-up

--- colt_land.mk
+++ colt_land.mk
@@ -21,6 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Inherit some common Colt stuff.
+$(call inherit-product, packages/apps/ColtApps/common.mk)
 
 TARGET_BOOT_ANIMATION_RES := 720x1280
 
