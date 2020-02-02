8076f355f0238a86e5f42d83e84f4529ae419af0
dianlujitao <dianlujitao@lineageos.org>
2020-02-02 19:33:37 +0800

    land: Explictly build camera HIDL interfaces
     * The 64-bit variants are used by IMS blobs but no longer automatically
       built along with the 32-bit counterparts.
    
    Change-Id: I387e4345c3088bbaf55e24bcfe286ccd877507a6

--- device.mk
+++ device.mk
@@ -137,6 +137,11 @@ PRODUCT_PACKAGES += \
     libmm-qcamera \
     GCam
 
+PRODUCT_PACKAGES += \
+    android.hardware.camera.device@3.4 \
+    android.hardware.camera.provider@2.5 \
+    vendor.qti.hardware.camera.device@1.0
+
 # Configstore
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-service
