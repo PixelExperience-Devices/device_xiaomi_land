894d28de72eb0f23e2b07f0186d1a44d9b8b1c20
JacobDeepu <d2jdeepu@gmail.com>
2020-03-14 20:52:22 +0530

    land: biometrics: Move to prebuild biometrics HAL
    - Build android.hardware.biometrics.fingerprint@2.1 from source

--- device.mk
+++ device.mk
@@ -194,7 +194,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.land
+    android.hardware.biometrics.fingerprint@2.1
 
 # FM
 PRODUCT_PACKAGES += \
