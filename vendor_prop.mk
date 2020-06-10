d7788d17eae932dacf747e1d5c1390ee883e20f1
Jacob Deepu <d2jdeepu@gmail.com>
2020-06-10 21:52:22 +0530

    land: Update media configs for Q.
    * From LA.UM.8.6.r1-04400-89xx.0
    * Remove unused configs.
    * Remove media.settings.xml prop.
    
    Change-Id: I240f7c1246256316c51cb604642ffc34480318de

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -109,7 +109,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.vidc.enc.narrow.searchrange=1 \
     vendor.vidc.disable.split.mode=1 \
     vendor.video.disable.ubwc=1 \
-    media.settings.xml=/vendor/etc/media_profiles_V1_0.xml \
     media.stagefright.thumbnail.prefer_hw_codecs=true
 
 # Netmgrd
