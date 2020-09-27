ef79453f65914d55eccd0daeb782064826a7b366
LuK1337 <priv.luk@gmail.com>
2020-09-27 17:45:33 +0200

    land: Use libcutils-v29.so for libdpmframework.so
    * v30 one doesn't have `strdup8to16` symbol in it.
    
    Change-Id: Id2b4bb3017bc24c817dbe4b03834960aa1a4b444

--- device.mk
+++ device.mk
@@ -418,7 +418,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v29.so \
-    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libcutils-v29.so
 
 # WiFi
 PRODUCT_PACKAGES += \
--- extract-files.sh
+++ extract-files.sh
@@ -66,6 +66,10 @@ fi
 function blob_fixup() {
     case "${1}" in
 
+    product/lib64/libdpmframework.so)
+        sed -i "s/libhidltransport.so/libcutils-v29.so\x00\x00\x00/" "${2}"
+        ;;
+
     vendor/bin/gx_fpcmd|vendor/bin/gx_fpd)
         patchelf --remove-needed "libbacktrace.so" "${2}"
         patchelf --remove-needed "libunwind.so" "${2}"
