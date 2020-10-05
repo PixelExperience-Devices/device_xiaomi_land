a542f01f55d9c15734436692df2abd4c528c9a98
Steven Moreland <smoreland@google.com>
2020-10-05 23:47:51 +0000

    land: camera: Disable camera memory init.
    Bug: 166675194
    Test: atest android.hardware.camera2.cts.PerformanceTest
    Change-Id: Ife2962e3ebbcd483f433045a4c5835660d3fb02c

--- device.mk
+++ device.mk
@@ -137,6 +137,10 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.5 \
     vendor.qti.hardware.camera.device@1.0
 
+# b/166675194
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.camera.provider24.disable_mem_init=1
+
 # Configstore
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-service
