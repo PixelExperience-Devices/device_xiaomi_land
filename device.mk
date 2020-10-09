478ed0002f3e9cc56cb93e1c466e6cae7700f548
Jacob Deepu <d2jdeepu@gmail.com>
2020-10-09 22:24:36 +0530

    land: Clean up Audio PRODUCT_PACKAGES
    * There isn't any reason to add these to PRODUCT_PACKAGES
    
    Change-Id: If612270292664f0e46f5e0912a5ee7536a61f3e0

--- device.mk
+++ device.mk
@@ -80,18 +80,10 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio@5.0 \
     android.hardware.audio@5.0-impl \
-    android.hardware.audio.common@5.0 \
-    android.hardware.audio.common@5.0-util \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@2.0-service \
-    android.hardware.audio.effect@5.0 \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.2-impl \
-    android.hardware.soundtrigger@2.2-service \
     audio.a2dp.default \
     audio.primary.msm8937 \
     audio.r_submix.default \
