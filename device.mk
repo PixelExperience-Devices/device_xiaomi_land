1117c411e992109b99977841ca54d7cbb83aba40
Volodymyr Zhdanov <wight554@gmail.com>
2020-11-18 21:45:41 +0100

    land: build vendor version of libavservices_minijail
    Change-Id: Ic5cde5de9dc5f003ef8436cc44606c32faa5beb5

--- device.mk
+++ device.mk
@@ -309,6 +309,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# Minijail
+PRODUCT_PACKAGES += \
+    libavservices_minijail.vendor
+
 # NET
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0 \
